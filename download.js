21a6eb089ee0a8b3d9de9bdf2588128cb2300af9
仁志 <legend1world@gmail.com>
2024-04-04 20:37:03 +0900

    up

---
+++ download.js
@@ -0,0 +1,8 @@
+import { parse } from "https://deno.land/std@0.79.0/encoding/csv.ts";
+
+const res = await fetch("https://www.data.jma.go.jp/stats/data/mdrr/pre_rct/alltable/pre3h00_rct.csv");
+const data = await res.text();
+
+const result = await parse(data);
+
+await Deno.writeTextFile("data/all.json", JSON.stringify(result));
