ac0207cdf5623094a235e1cf16f2cfb7980723e5
仁志 <legend1world@gmail.com>
2024-04-14 16:52:24 +0900

    up

--- download.js
+++ download.js
@@ -1,8 +1,5 @@
-import { parse } from "https://deno.land/std@0.79.0/encoding/csv.ts";
+import { CSV } from "https://js.sabae.cc/CSV.js";
 
-const res = await fetch("https://www.data.jma.go.jp/stats/data/mdrr/pre_rct/alltable/pre3h00_rct.csv");
-const data = await res.text();
+const data = await CSV.fetch("https://www.data.jma.go.jp/stats/data/mdrr/pre_rct/alltable/pre3h00_rct.csv");
 
-const result = await parse(data);
-
-await Deno.writeTextFile("./data/all.json", JSON.stringify(result));
+await Deno.writeTextFile("./data/all.csv", CSV.encode(data));
