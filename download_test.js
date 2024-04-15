dcf8436c368cacd8fd67f47d9794fc26607f6e46
仁志 <legend1world@gmail.com>
2024-04-15 19:33:44 +0900

    up

--- download_test.js
+++ download_test.js
@@ -1,18 +1,12 @@
+import { CSV } from "https://js.sabae.cc/CSV.js";
 import { parse } from "https://deno.land/std@0.79.0/encoding/csv.ts";
 import { BufReader } from "https://deno.land/std@0.79.0/io/bufio.ts";
 import { basename, dirname, extname } from "https://deno.land/std@0.130.0/path/mod.ts";
 
-// CSV ファイルの URL
-const url = "https://www.data.jma.go.jp/stats/data/mdrr/pre_rct/alltable/pre3h00_rct.csv";
+const data = await CSV.fetch("https://www.data.jma.go.jp/stats/data/mdrr/pre_rct/alltable/pre3h00_rct.csv");
 
-// CSV ファイルを取得
-const res = await fetch(url);
-const raw = await res.arrayBuffer();
-const decoder = new TextDecoder("shift_jis");
-const rawString = decoder.decode(raw);
+await Deno.writeTextFile("./data/all.csv", CSV.encode(data));
 
-
-await Deno.writeTextFile("./data/all.csv", rawString);
 
 const file = await Deno.open("./data/all.csv");
 
