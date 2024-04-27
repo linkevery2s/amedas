58d54c98d1a4c46c96035669d15ee3237e099210
仁志 <legend1world@gmail.com>
2024-04-27 17:58:20 +0900

    up

--- download.js
+++ download.js
@@ -124,3 +124,28 @@ try {
 } finally {
   file5.close();
 }
+
+// last update
+let dt = new Date();
+
+let ts = dt.getTime();
+
+let ts_after = ts + (1000 * 60 * 60 * 9);
+
+let date = new Date(ts_after);
+
+console.log(date);
+
+const year = date.getFullYear();
+
+const month = date.getMonth() + 1;
+
+const day = date.getDate();
+
+const hour = date.getHours();
+
+const minute = date.getMinutes();
+
+const now_day = year + "年" + month + "月" + day + "日" + hour + "時" + minute + "分";
+
+const write = Deno.writeTextFile("./data/lastupdate.json", '[{"lastupdate":' + '"' + now_day + '"}]');
